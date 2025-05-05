dc9e45faab1d07bf3422ce858de3549df28473c7
AlexS <schwimmeralexander@gmail.com>
2025-05-04 20:37:33 -0700

    Update project o use latest PDFBox version 3.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.apache.pdfbox/pdfbox
-    implementation("org.apache.pdfbox:pdfbox:2.0.24")
+    implementation("org.apache.pdfbox:pdfbox:3.0.5")
     testImplementation ("org.assertj:assertj-core:3.23.0")
 
 }
